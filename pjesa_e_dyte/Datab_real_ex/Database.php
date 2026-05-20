43bba82e9fcbaf7469590e30d6f1ed15c4fc1e37
bletadoci <bleta.doci@student.uni-pr.edu>
2026-05-20 15:45:42 +0200

    class addition

--- pjesa_e_dyte/Datab_real_ex/Database.php
+++ pjesa_e_dyte/Datab_real_ex/Database.php
@@ -11,7 +11,7 @@
         $this->conn = new PDO("mysql: host={$this->host}, dbname={$this->dbname}, $this->username, $this->password", charset={this->charset});
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, ERRMODE_EXCEPTION);
         this->conn-> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        //setAttribute(PDO:: dbname, "dbname")
+        //setAttribute(PDO:: dbname, PDO::"dbname")
         }
         catch(PDOException $e){
             die("Connection failed". $e.getMessage());
